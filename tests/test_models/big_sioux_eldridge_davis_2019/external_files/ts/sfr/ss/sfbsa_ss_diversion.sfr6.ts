18a1bc4b48296d9533f0bea03646c01d9b4aab86
Stephen Hundt <shundt@usgs.gov>
2025-04-11 16:16:05 -0600

    Add tests dir and test models

---
+++ tests/test_models/big_sioux_eldridge_davis_2019/external_files/ts/sfr/ss/sfbsa_ss_diversion.sfr6.ts
@@ -0,0 +1,10 @@
+BEGIN ATTRIBUTES
+  NAMES  airport  baltic  bigditch  wetland_pump
+  METHOD  STEPWISE
+END ATTRIBUTES
+
+BEGIN TIMESERIES
+ #  time    airport     baltic  bigditch  wetland_pump 
+    0       32513658.8  0       0         133881.1
+    38352   32513658.8  0       0         133881.1
+END TIMESERIES       
